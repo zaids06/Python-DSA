6edb0fc67d42fee48c74029306f188ec2be7fec7
Zaid Shah <99684512+zaids06@users.noreply.github.com>
2023-12-24 07:28:48 +0530

    Recursion Practice problem

---
+++ recursion_practice_problems/PairStar.py
@@ -0,0 +1,8 @@
+def pairStar(str):
+    if len(str) < 2:
+        return str
+    if str[0] ==str[1]:
+        return str[0]+"*"+pairStar(str[1:])
+    return str[0]+pairStar(str[1:])
+string=input()
+print(pairStar(string))
