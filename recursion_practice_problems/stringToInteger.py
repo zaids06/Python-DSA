17094c66f151cdb08c706d7d2b775ba0be318fba
Zaid Shah <99684512+zaids06@users.noreply.github.com>
2023-12-24 07:23:02 +0530

    Recursion Practice

--- recursion_practice_problems/stringToInteger.py
+++ recursion_practice_problems/stringToInteger.py
@@ -5,5 +5,5 @@ def stringToInteger(s):
     mul=10**(length-1)
     return int(s[0])*mul+stringToInteger(s[1:])
 
-string=input()
-print(stringToInteger(string))
+str=input()
+print(stringToInteger(str))
