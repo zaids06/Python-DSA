78afda98adfd1f9c0f7852c8deb60109dd5ce5ea
Zaid Shah <99684512+zaids06@users.noreply.github.com>
2024-01-15 21:25:56 +0530

    basic class program

---
+++ ObjectOrientedProgramming/practice.py
@@ -0,0 +1,17 @@
+class MyClass:
+    def __init__(self):
+        pass
+
+    def get_max_value(self,x, y):
+        # write you code logic !!
+        if x >= y:
+            return x
+        else:
+            return y
+
+
+# Create an instance of MyClass
+x = int(input())
+y = int(input())
+obj = MyClass()
+obj.get_max_value(x, y)
