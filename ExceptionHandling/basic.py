88c75913d5caf00e40a3b352a9e01f7b077a23fa
Zaid Shah <99684512+zaids06@users.noreply.github.com>
2024-02-03 23:57:21 +0530

    Practice

---
+++ ExceptionHandling/basic.py
@@ -0,0 +1,7 @@
+try:
+    a = 10
+    b = 10
+    c = a / b
+    print(c)
+except ZeroDivisionError:
+    print("ZeroDivisionError")
---
+++ ExceptionHandling/division.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+try:
+    print(a//b)
+except ZeroDivisionError:
+    print("Division not possible")
+print("Hey you are doing division")
