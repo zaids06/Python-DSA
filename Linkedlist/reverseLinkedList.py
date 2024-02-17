2a0899cdd6fbe2e8eca09b3563e41971ec7c1394
Zaid Shah <99684512+zaids06@users.noreply.github.com>
2024-02-17 20:05:34 +0530

    reverse Linked List

---
+++ Linkedlist/reverseLinkedList.py
@@ -0,0 +1,72 @@
+from sys import stdin, setrecursionlimit
+
+setrecursionlimit(10 ** 6)
+
+
+# Following is the Node class already written for the Linked List
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def printReverse(head):
+    # Your code goes here
+    if head is None or head.next is None:
+        return head
+    prev = head
+    cur = head.next
+    prev.next = None
+    temp = head
+    while cur is not None:
+        temp = cur.next
+        cur.next = prev
+        prev = cur
+        cur = temp
+    head=prev
+    return head
+
+
+# Taking Input Using Fast I/O
+def takeInput():
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1):
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None:
+            head = newNode
+            tail = newNode
+
+        else:
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+# to print the linked list
+def printLinkedList(head):
+    while head is not None:
+        print(head.data, end=" ")
+        head = head.next
+
+    print()
+
+
+# main
+t = int(stdin.readline().rstrip())
+
+while t > 0:
+    head = takeInput()
+    head=printReverse(head)
+    printLinkedList(head)
+
+    t -= 1
